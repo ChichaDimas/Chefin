7172f448338df5b8ddb5aa63e6a7ea57a8179230
vilka <vilkamil9@gmail.com>
2023-05-05 00:02:58 +0300

    пор

--- chefin/store/models.py
+++ chefin/store/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128,blank=True)
+    category = models.CharField(max_length=128,blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(upload_to='products_images', blank=True)
+
+
+    def __str__(self):
+        return self.name
--- chefin/store/views.py
+++ chefin/store/views.py
@@ -1,58 +1,61 @@
+import os
+
 from django.shortcuts import render
 import requests
-from django.conf import settings
-from requests import Response
-
 from chefin.settings import POSTER_POS_API_KEY, POSTER_VENUE_ID
-from django.http import JsonResponse, HttpResponseRedirect
-import json
 from cloudipsp import Api, Checkout
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_POST
+from store.cart import Cart
+from .models import Product
+from django.core.files.base import ContentFile
+from urllib.request import urlretrieve
+
+import requests
+from urllib.parse import urlparse
 
 
 
-def get_menu_categories(api_key):
-    url = 'https://joinposter.com/api/menu.getProducts'
+from django.shortcuts import render
+from .helpers import *
 
-    params = {
-        'token': api_key,
-        'format': 'json'
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        return response.json().get('response')
-    return []
+# def menu(request):
+#     api_key = POSTER_POS_API_KEY
+#     fill_database(api_key)
+#
+#     return render(request, 'store/menu.html', {})
 
 def menu(request):
-    categories = get_menu_categories(POSTER_POS_API_KEY)
-    photo = []
-    price = []
-
-    for category in categories:
-        if category.get('price'):
-            price_str = category['price']['1']
-            price_float = float(price_str) / 100
-            price_formatted = round(price_float, 2)
-            price.append(price_formatted)
+    api_key = POSTER_POS_API_KEY
+    fill_database(api_key)
+    categories = Product.objects.all()
+    context = {'categories': categories}
+    return render(request,'store/menu.html',context)
 
 
-    for category in categories:
-        if category.get('photo'):
-            photo.append(f"https://joinposter.com{category['photo']}")
 
-    for i, category in enumerate(categories):
-        if category.get('photo'):
-            category['photo'] = photo[i]
-
-        if category.get('price'):
-            category['price'] = price[i]
-
-    context = {
-        'categories': categories,
-        'title': 'Hellobro',
-
-    }
-    return render(request, 'store/menu.html', context)
-
+# def get_menu_categories(api_key):
+#     url = 'https://joinposter.com/api/menu.getProducts'
+#
+#     params = {
+#         'token': api_key,
+#         'format': 'json'
+#     }
+#     response = requests.get(url, params=params)
+#     if response.status_code == 200:
+#         return response.json().get('response')
+#     return []
+#
+# def menu(request):
+#     categories = get_menu_categories(POSTER_POS_API_KEY)
+#
+#     context = {
+#         'categories': categories,
+#         'title': 'Hellobro',
+#
+#     }
+#     return render(request, 'store/menu.html', context)
 
 
 def add_to_cart(request):
@@ -61,10 +64,9 @@ def add_to_cart(request):
     checkout = Checkout(api=api)
     data = {
         "currency": "UAH",
-        "amount": 300
+        "amount": 12000
     }
     url = checkout.url(data).get('checkout_url')
-    print(url)
     context = {
         'title':'Store',
         'url': url
@@ -73,19 +75,6 @@ def add_to_cart(request):
 
 
 
-# def basket_add(request,product_id):
-#     product = menu.request.get(id=product_id)
-#     baskets = Basket.objects.filter(user=request.user,product=product)
-#
-#     if not baskets.exists():
-#         Basket.objects.create(user=request.user,product=product,quantity=1)
-#     else:
-#         basket = baskets.first()
-#         basket.quantity+=1
-#         basket.save()
-#
-#     return HttpResponseRedirect(request.META['HTTP_REFERER'])
-
 
 def index(request):
     context = {
@@ -93,22 +82,5 @@ def index(request):
     }
     return render(request,'store/index.html',context)
 
-
-
-def pay(request):
-
-    api = Api(merchant_id=1397120,
-          secret_key='Not for tests. Test credentials: https://docs.fondy.eu/docs/page/2/ ')
-    checkout = Checkout(api=api)
-    data = {
-        "currency": "UAH",
-        "amount": 300
-        }
-    url = checkout.url(data).get('checkout_url')
-    print(url)
-    context = {
-        'url': url
-    }
-    return render(request, 'store/payment.html', context)
 
 
