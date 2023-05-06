cf984f5ea30dc61d568ae3e5c41605c2a35900ac
Delibero69 <deliberin@gmail.com>
2023-05-06 17:20:56 +0300

    Merge remote-tracking branch 'origin/main'

--- chefin/store/models.py
+++ chefin/store/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=128,blank=True)
     category = models.CharField(max_length=128,blank=True)
     description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
     image = models.ImageField(upload_to='products_images', blank=True)
 
 
